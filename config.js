f359ad37a882fe88dc4186e5546a2c6acea1d600
jinkyungE <jinkyung1134@naver.com>
2026-05-30 06:07:06 +0900

    Add config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+window.SiteConfig = {
+  supabase: {
+    url: "https://akvchomievmiekxwlkrr.supabase.co",
+    anonKey: "sb_publishable_0TuKcHxMFSx8-slGY_0qpw_koSG2c5i",
+    storageBucket: "noir-case-assets",
+  },
+  payment: {
+    provider: "demo",
+    tossClientKey: "",
+  },
+  currency: {
+    base: "KRW",
+    rates: {
+      KRW: 1,
+      USD: 0.00073,
+      CNY: 0.00525,
+    },
+  },
+};
